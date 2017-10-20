5658adce947c1e0db990e1ce6fda4756141a6bd5
Lain <0xlain@protonmail.com>
2017-10-20 23:55:46 +0200

    feat(snake:shortening): merge window creation in the map just next to it.
    -3 chars

--- snake/1_line_snake.py
+++ snake/1_line_snake.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 while"_"in globals()or(map(globals().__setitem__,"lain_snake",[(640,480),1,1,1,
-10,globals().__setitem__,tuple]+map(__import__,("pygame","random","time"))),s(
+10,globals().__setitem__,tuple]+map(__import__,("pygame","random","time"))),s( 
 "P",lambda:x and I()or(i or(s("b",b[1:]+[o]),C(b[0],0),s("o",n(o[j]+d[j]*_ for 
 j in[0,1])),(s("x",o in b or any(not 0<=o[j]<=l[j]-_ for j in[0,1])))or x or C(
 o)),[g.type==12 and exit()or g.type==2 and K(g.key)for g in a.event.get()],o==f
-and(b.append(f),F()),e.sleep(0.05))),s("I",lambda:(s("y",a.display),a.init(),s(
-"w",y.set_mode(l)),map(s,"ixbdo",(1,0,[],(0,0),n(x/2-x/2%_ for x in l))),C(o),F
-(),y.flip())),s("F",lambda:(s("f",n(k.randrange(0,x-1,_)for x in l)),C(f,255<< 
-16))),s("C",lambda r,c=0xff00,h=1:h and C(r+(_,)*2,c,0)or(w.subsurface(r).fill(
-c),y.update(r))),s("K",lambda k:(0<k-272<5 and(lambda v:(v[0]+d[0]or v[1]+d[1])
-and s("d",v))(((0,-1),(0,1),(1,0),(-1,0))[k-273]),s("i",k==32))),I()):P()
+and(b.append(f),F()),e.sleep(0.05))),s("I",lambda:(a.init(),s("y",a.display),  
+map(s,"ixbdow",(1,0,[],(0,0),n(x/2-x/2%_ for x in l),y.set_mode(l))),C(o),F(), 
+y.flip())),s("F",lambda:(s("f",n(k.randrange(0,x-1,_)for x in l)),C(f,255<<16))
+),s("C",lambda r,c=0xff00,h=1:h and C(r+(_,)*2,c,0)or(w.subsurface(r).fill(c),y
+.update(r))),s("K",lambda k:(0<k-272<5 and(lambda v:(v[0]+d[0]or v[1]+d[1])and 
+s("d",v))(((0,-1),(0,1),(1,0),(-1,0))[k-273]),s("i",k==32))),I()):P()
