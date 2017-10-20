40ec13cddc112a0d13a26e62837a46075976c19a
Lain <0xlain@protonmail.com>
2017-10-20 14:46:16 +0200

    feat(snake:shorten): remove double check for space keydown
    Nothing more.

--- snake/1_line_snake.py
+++ snake/1_line_snake.py
@@ -9,6 +9,5 @@ and(b.append(f),F()),e.sleep(0.05))),s("I",lambda:(s("y",a.display),a.init(),s(
 "w",y.set_mode(l)),map(s,"ixbdo",(1,0,[],(0,0),n(x/2-x/2%_ for x in l))),C(o),F
 (),y.flip())),s("F",lambda:(s("f",n(k.randrange(0,x-1,_)for x in l)),C(f,255<< 
 16))),s("C",lambda r,c=0xff00,h=1:h and C(r+(_,)*2,c,0)or(w.subsurface(r).fill(
-c),y.update(r))),s("K",lambda k:0<k-272<5 and[(lambda v:(v[0]+d[0]or v[1]+d[1])
-and(s("d",v),s("i",0)))(((0,-1),(0,1),(1,0),(-1,0))[k-273])]or s("i",k==32)),I(
-)]):P()
+c),y.update(r))),s("K",lambda k:(0<k-272<5 and(lambda v:(v[0]+d[0]or v[1]+d[1])
+and s("d",v))(((0,-1),(0,1),(1,0),(-1,0))[k-273]),s("i",k==32))),I()]):P()
