6b25b0bb9dd1ad724c6123f861cd00f6c2ef92c2
Lain <0xlain@protonmail.com>
2018-04-19 14:34:18 +0200

    feat(GameOfLife:creation): finished
    .

---
+++ GoL/GameOfLife.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+while("G"in globals())or[*map(globals().__setitem__,"GolByLain_svk",(
+globals().__setitem__,range,enumerate,*map(__import__,("time","pygame"
+,"random")),lambda:(B.sleep(0.01),[{12:exit,5:i,2:n}.get(e.type,k)(e)
+for e in y.event.get()],P or G("C",[[sum(1 for c in(x+1,x,x-1)for l in
+(y+1,y,y-1)if X>c>=0 and Y>l>=0 and C[l][c]and(c,l)!=(x,y))in U[C[y][x
+]]for x in o(X)]for y in o(Y)]),F.fill(0),[a and F.blit(W,(x*S,y*S))
+for y,r in l(C)for x,a in l(r)],D.flip()),lambda m:{1:_,3:s}.get(m.
+button,k)(m.pos[0]//S,m.pos[1]//S),lambda k:k.key==32 and G("P",~P),
+lambda x,y:C[y].__setitem__(x,not C[y][x]),lambda*_:G("C",[[L.randint(
+0,1)for x in o(X)]for y in o(Y)]),lambda:(y.init(),[*map(G,"PSRUD",(-1
+,8,(640,480),{1:{2,3},0:{3}},y.display))],G("X",R[0]//S),G("Y",R[1]//S
+),s(),G("F",D.set_mode(R)),G("W",y.Surface((S,S))),W.fill((255,)*3)),
+lambda*_:0))]and v():a()
